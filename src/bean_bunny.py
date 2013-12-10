fa337a0c58feef86a7d94365ae91044196bd51e3
Brandon High <brandon.high@puppetlabs.com>
2013-12-09 16:36:01 -0800

    A few more search commands, new help page.

--- src/bean_bunny.py
+++ src/bean_bunny.py
@@ -83,11 +83,11 @@ class BeanCommands(bunny1.Bunny1Commands):
     r = redmine
 
     def printing(self, arg):
-        """How to print"""
+        """HOW-TO print"""
         return "https://puppetlabs.atlassian.net/wiki/display/OPS/Printer+Setup"
 
     def phone(self, arg):
-        """How to phones"""
+        """HOW-TO phone"""
         return "https://puppetlabs.atlassian.net/wiki/display/OPS/ShoreTel+Phone"
     phones = phone
 
@@ -109,7 +109,7 @@ class BeanCommands(bunny1.Bunny1Commands):
     hc = hipchat
 
     def fax(self, arg):
-         """How to fax"""
+         """HOW-TO fax"""
          return "https://sites.google.com/a/puppetlabs.com/main/home/basic-business-practices/howto-send-and-get-a-fax"
 
     def gmail(self, arg):
@@ -133,18 +133,149 @@ class BeanCommands(bunny1.Bunny1Commands):
             return "https://drive.google.com/a/puppetlabs.com/?tab=mo#my-drive"
     gd = gdrive
 
+    def trello(self, arg):
+        """Open/Search Trello board"""
+        if len(arg) > 0:
+            return "https://trello.com/search?q=%s" % arg
+        else:
+            return "https://trello.com/puppetlabs"
+    tr = trello
     # ... and you can add other commands by just defining more methods
     # in this class here
+
+    def puppet_docs(self, arg):
+        """Open/Search Puppet docs"""
+        if len(arg) > 0:
+            return "https://www.google.com/#q=site:docs.puppetlabs.com+%s" % qp(arg)
+        else:
+            return "http://docs.puppetlabs.com/"
+    pd = puppet_docs
+
+    def puppet_forge(self, arg):
+        """Open/Search Puppet forge"""
+        if len(arg) > 0:
+            return "http://forge.puppetlabs.com/modules?q=%s" % qp(arg)
+        else:
+            return "http://forge.puppetlabs.com/"
+    pf = puppet_forge
+
+    @dont_expose
+    def _help_html(self, examples=None, name="bean_bunny"):
+        """the help page that gets shown if no command or 'help' is entered
+        This horrible return would probably be better suited as a template.
+        """
+
+        import random
+
+        def bookmarklet(name):
+            return """<a href="javascript:bunny1_url='""" + self._base_url() + """?';cmd=prompt('bunny1.  type &quot;help&quot; to get help or &quot;list&quot; to see commands you can use.',window.location);if(cmd){window.location=bunny1_url+escape(cmd);}else{void(0);}">""" + name + """</a>"""
+
+        if not examples:
+            examples = [
+                    "g whizbang api reference",
+                    "j PE-1234",
+                    "c onboarding",
+                    ]
+
+        return """
+<html>
+<head>
+<title>bean_bunny</title>
+""" + self._opensearch_link() + """
+<style>
+BODY {
+    font-family: Sans-serif;
+    width: 800px;
+}
+
+code {
+    color: darkgreen;
+}
+
+A {
+    color: #3B5998;
+}
+
+small {
+    width: 800px;
+    text-align: center;
+}
+
+.header {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 30px;
+}
+
+.test-query-input {
+    width: 487px;
+    font-size: 20px;
+}
+
+.header-placeholder {
+    height: 45px;
+}
+
+</style>
+</head>
+<body>
+<h1 class="header-placeholder"><img class="header" src="header.png" /></h1>
+
+<p>""" + name + """ is a tool that lets you write smart bookmarks in python and then share them across all your browsers and with a group of people or the whole world.  It was developed at <a href="http://www.facebook.com/">Facebook</a> and is widely used there.</p>
+
+<form method="GET">
+<p style="width: 820px; text-align: center;"><input class="test-query-input" id="b1cmd" type="text" name="___" value=""" + '"' + escape(random.choice(examples)) + '"' + """/> <input type="submit" value=" try me "/></p>
+
+<p>Type something like """ + " or ".join(["""<a href="#" onclick="return false;"><code onclick="document.getElementById('b1cmd').value = this.innerHTML; return true;">""" + x + "</code></a>" for x in examples]) + """.</p>
+
+<p>Or you can see <a href="?list">a list of shortcuts you can use</a> with this example server.</p>
+
+<h3>Installing on Google Chrome</h3>
+<ul>Choose <code>Options</code> from the wrench menu to the right of the location bar in Chrome, then under the section <code>Default Search</code>, click the <code>Manage</code> button.  Click the <code>Add</code> button and then fill in the fields name, keyword, and URL with <code>""" + name + """</code>, <code>b1</code>, and <code>""" + self._base_url() + """?</code>.  Hit <code>OK</code> and then select """ + name + """ from the list of search engines and hit the <code>Make Default</code> button to make """ + name + """ your default search engine.  Type <code>list</code> into your location bar to see a list of commands you can use.</ul>
+
+<h3>Installing on Firefox</h3>
+<ul>Type <code>about:config</code> into your location bar in Firefox.</ul>
+<ul>Set the value of keyword.URL to be <code>""" + self._base_url() + """?</code></ul>
+<ul>Make sure you include the <code>http://</code> at the beginning and the <code>?</code> at the end.</ul>
+<ul>Now, type <code>list</code> or <code>wp FBML</code> into your location bar and hit enter.</ul>
+<ul>Also, if you are a Firefox user and find bunny1 useful, you should check out <a href="http://labs.mozilla.com/projects/ubiquity/">Ubiquity</a>.</ul>
+
+<h3>Installing on Safari</h3>
+<ul>Drag this bookmarklet [""" + bookmarklet(name) + """] to your bookmarks bar.</ul>
+<ul>Now, visit the bookmarklet, and in the box that pops up, type <code>list</code> or <code>g facebook comments widget video</code> and hit enter.</ul>
+<ul>In Safari, one thing you can do is make the bookmarklet the leftmost bookmark in your bookmarks bar, and then use <code>Command-1</code> to get to it.</ul>
+<ul>Alternatively, you can get the location bar behavior of Firefox in Safari 3 by using the <a href="http://purefiction.net/keywurl/">keywurl</a> extension.</ul>
+
+<h3>Installing on Internet Explorer</h3>
+<ul>There aren't any great solutions for installing """ + name + """ on IE, but two OK solutions are:</ul>
+<ul>You can use this bookmarklet [""" + bookmarklet(name) + """] by dragging into your bookmarks bar and then clicking on it when you want to use """ + name + """.</ul>
+<ul>Or, in IE7+, you can click the down arrow on the search bar to the right of your location bar and choose the starred """ + name + """ option there.  This will install the bunny OpenSearch plugin in your search bar.</ul>
+
+<h3>Running Your Own bunny1 Server</h3>
+<ul>Download the <a href="http://github.com/ccheever/bunny1/">source code</a> for the project.  Or if you use setuptools, you can just <code>easy_install bunny1</code>.</ul>
+
+<ul>To run an example server, just run <code>b1_example.py --port=8080</code>.</ul>
+
+<ul>More detailed instructions for configuring and running your own server can be found in the <a href=""" + '"' + self._base_url() + """?readme">README</a>.  You can add your own commands with just a few lines of python.</ul>
+
+<hr />
+<small>bunny1 was originally written by <a href="http://www.facebook.com/people/Charlie-Cheever/1160">Charlie Cheever</a> at <a href="http://developers.facebook.com/opensource.php">Facebook</a> and is maintained by him, <a href="http://www.facebook.com/people/David-Reiss/626221207">David Reiss</a>, Eugene Letuchy, and <a href="http://www.facebook.com/people/Daniel-Corson/708561">Dan Corson</a>.  Julie Zhuo drew the bunny logo.</small>
+
+
+</body>
+</html>
+        """
 
 class BeanBunny(bunny1.Bunny1):
     def __init__(self):
         bunny1.Bunny1.__init__(self, BeanCommands(), bunny1.Bunny1Decorators())
 
     @cherrypy.expose
-    def header_gif(self):
-        """the banner GIF for the bunny1 homepage"""
-        cherrypy.response.headers["Content-Type"] = "image/gif"
-        return bunny1.bunny1_file("header.gif")
+    def header_png(self):
+        """the banner PNG for the bunny1 homepage"""
+        cherrypy.response.headers["Content-Type"] = "image/png"
+        return bunny1.bunny1_file("header.png")
 
 if __name__ == "__main__":
     bunny1.main(BeanBunny())
